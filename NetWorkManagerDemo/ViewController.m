7385c1ef6598d8864fc36c246b36b05c172e04aa
jack <743285571@qq.com>
2016-08-16 16:48:28 +0800

    移除cocopods依赖

---
+++ NetWorkManagerDemo/ViewController.m
@@ -0,0 +1,38 @@
+//
+//  ViewController.m
+//  NetWorkManagerDemo
+//
+//  Created by jack on 16/8/16.
+//  Copyright © 2016年 jack. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "CCNetWorkManager.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    //输入你的请求地址和请求参数
+    [CCNetWorkManager GETRequestWithUrl:@"" params:@{} success:^(NSDictionary *responseData) {
+        
+    } failure:^(NSError *requestError) {
+        
+    } progress:^(NSProgress *progress) {
+        
+    }];
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
--- Pods/Target Support Files/Pods-CCNetWorkManager-CCNetWorkManagerTests/Pods-CCNetWorkManager-CCNetWorkManagerTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_CCNetWorkManager_CCNetWorkManagerTests : NSObject
-@end
-@implementation PodsDummy_Pods_CCNetWorkManager_CCNetWorkManagerTests
-@end
